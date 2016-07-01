dc831bd5239273e84c32c9f5ae9a854dda6509e1
keziah <k.milligan.1@research.gla.ac.uk>
2016-07-01 16:57:56 +0100

    rewritten bibCite using regular expressions

---
+++ bibCite2.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 14:44:03 2016
+
+@author: keziah
+"""
+
+import sys
+import re
+
+class bibCite():
+    ''' Replace citations in a .tex file with the correct \cite{} command.
+    
+        It will look for citations of the form 'Author Year', 
+        'Author1 & Author2 Year' or 'Author1 et al. Year' (where '&' may be 
+        '\&' or 'and' and 'et al.' may or may not have a full stop.)
+    
+        Parameters:
+            
+            bibFile: .bib file
+            
+            texFile: .tex document
+            
+            outFile (optional): .tex file to write to. If None, the input .tex 
+                                file will be overwritten
+    '''
+    
+    def __init__(self, sysArgv):
+        
+        self.bibFile = sysArgv[1]
+
+        self.texFile = sysArgv[2]
+    
+        if len(sysArgv) == 4:
+            self.outFile = sysArgv[3]
+    
+        else:
+            self.outFile = sysArgv[2]
+
+        # fill dictionary of citations and citeIDs
+        self.readBibFile()
+
+        # replace citations
+        self.replaceCitations()
+            
+
+    def readBibFile(self):
+        
+        f = open(self.bibFile, 'r')
+        
+        self.bib = f.read()
+        
+        f.close()
+        
+        # dictionary of strings to look for in the text (keys) and the bibtex
+        # IDs (values) with which to replace them
+        self.citations = {}
+
+        bibEntries = re.finditer(r'(@\w+\{)', self.bib)
+        
+        # indices of beginning of each entry
+        indices = list(b.span()[0] for b in bibEntries)
+        
+        for i in range(len(indices)-1):
+            
+            # create string for each entry
+            s = self.bib[indices[i]:indices[i+1]]
+
+            # ignore comments
+            if s[:8] != '@comment':
+
+                citeID = s[s.index('{')+1:s.index(',')]
+
+                athr = re.search(r'(author *= *\{.+\})', s)
+                
+                if athr is None:
+                    athr = re.search(r'(editor *= *\{.+\})', s)
+
+                authorStr = s[s.index('{', athr.span()[0]):athr.span()[1]-1]
+
+                # find the number of authors
+                authorCount = authorStr.split().count('and') + 1
+
+                # create correct author list to look for in the text
+                if authorCount == 1:
+                    commaIdx = authorStr.find(',')
+                    author = [authorStr[1:commaIdx]]
+
+                elif authorCount == 2:
+                    commaIdx = authorStr.find(',')
+                    author1 = authorStr[1:commaIdx]
+
+                    andIdx = authorStr.find(' and ') + 5
+                    commaIdx = authorStr.find(',', andIdx)
+                    author2 = authorStr[andIdx:commaIdx]
+
+                    author = ['{} \& {}'.format(author1, author2),
+                              '{} & {}'.format(author1, author2),
+                              '{} and {}'.format(author1, author2)]
+                    
+                elif authorCount > 2:
+                    commaIdx = authorStr.find(',',)
+                    author1 = authorStr[1:commaIdx]
+                    author = ['{} et al.'.format(author1), 
+                              '{} et al'.format(author1)]
+
+                yr = re.search(r'(year *= *\{.+\})', s)
+                
+                year = s[s.index('{', yr.span()[0])+1:yr.span()[1]-1]
+
+                for a in author:
+                    # create citation
+                    citation = '{} {}'.format(a, year)
+                    
+                    # if this citation is already in the dictionary, add 'a'  
+                    # and 'b' to the citations
+                    if citation in self.citations.keys():
+                        
+                        # rename first instance
+                        rename = ''.join([citation, 'a'])
+                        
+                        self.citations[rename] = self.citations[citation]
+                        
+                        # remove old citation
+                        self.citations.pop(citation)
+                        
+                        # create new citation
+                        citation = ''.join([citation, 'b'])
+                        
+                    # add citation and ID to dictionary
+                    self.citations[citation] = citeID
+
+
+    def replaceCitations(self):
+        
+        f = open(self.texFile, 'r')
+        
+        tex = f.read()
+        
+        f.close()
+        
+        for k in self.citations.keys():
+            # replacement string
+            cite = '\cite{{{}}}'.format(self.citations[k])
+            
+            replace = re.finditer(k, tex)
+            
+            indices = list(r.span() for r in replace)
+            
+            for i in reversed(indices):
+                # replace citation with \cite{ID}
+                tex = ''.join([tex[:i[0]], cite, tex[i[1]:]])
+                
+        # write new .tex file
+        f = open(self.outFile, 'w')
+            
+        f.write(tex)
+        
+        f.close()  
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:
+        
+        bibCite(['', 'text2bib_output.bib', 'smr_paper.tex', 'out.tex'])
+        
+    elif len(sys.argv) >= 3:
+        
+        bibCite(sys.argv)
